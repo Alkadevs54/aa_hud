e11f9ded3401541ed1949226a4ecfaf04cd41f6f
Alkadevs <97219545+Alkadevs54@users.noreply.github.com>
2026-01-08 12:35:24 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Alkadevs'
+ui_page 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'new-postals.json'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
